7e5b2570b219de3eba6b8b8a6f063a0f26309948
arpannappy26 <127881374+arpannappy26@users.noreply.github.com>
2023-03-14 22:53:58 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>  //header file to call standard input output library
+int main()         // here int is a keyword and main represents function
+{ //writing the main function of the program
+    printf("Hello World");
+    return 0;
+}

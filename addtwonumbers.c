0e290dc2c02a0873c321e2b3c20f5b0ec7753f5d
arpannappy26 <127881374+arpannappy26@users.noreply.github.com>
2023-03-14 23:40:44 +0530

    Update addtwonumbers.c

--- addtwonumbers.c
+++ addtwonumbers.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> //header file
 int main()
 {
-    int a, b;
-    printf("Enter the value of a\n");
-    scanf("%d", &a);
+    int a, b; //declaring variables a and b
+    printf("Enter the value of a\n"); //to print statement to user
+    scanf("%d", &a); //to take input value of a from user
     
-    printf("Enter the value of b\n");
-    scanf("%d", &b);
+    printf("Enter the value of b\n"); //to print statement to user
+    scanf("%d", &b); //to take input value of b from user
     
-    printf("The Sum is %d\n",a+b);
+    printf("The Sum is %d\n",a+b); //final result of the program
+    return 0;
 }

bea15bc505847e7919fd2d4970b9f20880fbfe3e
arpannappy26 <127881374+arpannappy26@users.noreply.github.com>
2023-03-15 21:24:24 +0530

    Create divisionoftwointegers.c
    Program To Divide Two Integers

---
+++ divisionoftwointegers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{int x, y;
+ printf("Enter the value of x\n");
+ scanf("%d", &x);
+ 
+ printf("Enter the value of y\n");
+ scanf("%d", &y);
+ 
+ printf("The Result Is %d\n", x/y);
+ return 0;}
